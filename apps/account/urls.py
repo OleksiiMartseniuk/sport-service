4514593bc59c91c51d7effc470781b9562fdf266
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-04-30 16:26:00 +0300

    fix url path

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -6,5 +6,5 @@ from . import views
 api_urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
