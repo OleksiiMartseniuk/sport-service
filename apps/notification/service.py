fcf4303263436ca908e0d4307dd1af963c7554e8
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-06 11:38:56 +0300

    add filed html_message

--- apps/notification/service.py
+++ apps/notification/service.py
@@ -7,12 +7,14 @@ def create_notification(
     subject: str,
     group_notification: str,
     user_id: int | None = None,
+    html_message: str | None = None,
 ) -> Notification:
     return Notification.objects.create(
         user_id=user_id,
         recipient_email=recipient_email,
         subject=subject,
         message=message,
+        html_message=html_message,
         status=Notification.CREATE,
         group_notification=group_notification,
     )
