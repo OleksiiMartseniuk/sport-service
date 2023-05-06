7df010edc0f86e39ba0e0b6bee1f37752920561f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-06 20:42:00 +0300

    create model workout

--- apps/workout/admin.py
+++ apps/workout/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Workout
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title']
-    search_fields = ['title']
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'user',
+        'publish',
+        'created',
+    )
+    list_filter = (
+        'publish',
+        'user',
+        'created',
+    )
+    search_fields = ('title',)
