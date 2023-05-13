e8562836fdeff20556313be2d69b90c1f353f4e6
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-13 15:30:30 +0300

    create serializers Workout

--- apps/workout/serializers.py
+++ apps/workout/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from .models import Category
+from djoser.serializers import UserSerializer
+
+from .models import Category, Workout
 
 
 class CategorySerializers(serializers.ModelSerializer):
@@ -8,3 +10,35 @@ class CategorySerializers(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class WorkoutSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Workout
+        exclude = ['publish']
+
+
+class WorkoutListSerializers(WorkoutSerializers):
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+
+class WorkoutCreateSerializers(WorkoutSerializers):
+
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+    )
+
+
+class WorkoutUpdateSerializers(WorkoutSerializers):
+
+    class Meta:
+        model = Workout
+        fields = ['title']
+
+
+class WorkoutRetrieveSerializers(WorkoutSerializers):
+
+    category = CategorySerializers()
+    user = UserSerializer()
