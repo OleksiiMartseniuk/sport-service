f8336b46c79b9f235235617a6e08e760007407dd
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-13 15:30:44 +0300

    create workout urls

--- apps/workout/urls.py
+++ apps/workout/urls.py
@@ -3,6 +3,19 @@ from django.urls import path
 from . import views
 
 
+workout_list = views.WorkoutView.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+workout_detail = views.WorkoutView.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+
 api_urlpatterns = [
     path('categories/', views.CategoryView.as_view(), name='categories'),
+    path('workout/', workout_list, name='workout-list'),
+    path('workout/<int:pk>/', workout_detail, name='workout-detail'),
 ]
