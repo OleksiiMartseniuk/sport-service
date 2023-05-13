44d465886dc311fc036d48d25c4ddabb6d9d3ca5
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-13 15:30:06 +0300

    create  view WorkoutView

--- apps/workout/views.py
+++ apps/workout/views.py
@@ -1,10 +1,33 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
-from .models import Category
-from .serializers import CategorySerializers
+from .models import Category, Workout
+from .serializers import (
+    CategorySerializers,
+    WorkoutListSerializers,
+    WorkoutCreateSerializers,
+    WorkoutUpdateSerializers,
+    WorkoutRetrieveSerializers,
+)
+from .permissions import IsEditWorkout
 
 
 class CategoryView(generics.ListAPIView):
 
     serializer_class = CategorySerializers
     queryset = Category.objects.all().order_by('title')
+
+
+class WorkoutView(viewsets.ModelViewSet):
+
+    serializer_class = WorkoutListSerializers
+    queryset = Workout.objects.filter(publish=True).order_by('created')
+    permission_classes = [IsEditWorkout]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return WorkoutRetrieveSerializers
+        if self.action == 'create':
+            return WorkoutCreateSerializers
+        if self.action == 'update' or self.action == 'partial_update':
+            return WorkoutUpdateSerializers
+        return super().get_serializer_class()
