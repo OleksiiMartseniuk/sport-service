d4f19bc35f7669fdffc51cc11ec8d8f7af74ea60
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-19 21:23:31 +0300

    add user for ExerciseView

--- apps/workout/urls.py
+++ apps/workout/urls.py
@@ -13,9 +13,22 @@ workout_detail = views.WorkoutView.as_view({
     'patch': 'partial_update',
     'delete': 'destroy',
 })
+exercise_list = views.ExerciseView.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+exercise_detail = views.ExerciseView.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+
 
 api_urlpatterns = [
     path('categories/', views.CategoryView.as_view(), name='categories'),
     path('workout/', workout_list, name='workout-list'),
     path('workout/<int:pk>/', workout_detail, name='workout-detail'),
+    path('exercise/', exercise_list, name='exercise-list'),
+    path('exercise/<int:pk>/', exercise_detail, name='exercise-detail'),
 ]
