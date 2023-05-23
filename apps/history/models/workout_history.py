12fea209b2fe1c7dde4ed86fce5e944574381a76
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-05-23 18:38:56 +0300

    create model WorkoutHistory

---
+++ apps/history/models/workout_history.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from apps.workout.models import Workout
+
+
+class WorkoutHistory(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='history_workout',
+    )
+    workout = models.ForeignKey(
+        Workout,
+        on_delete=models.CASCADE,
+        related_name='history_workout',
+    )
+    # [{'datetime': 'event'}]
+    detail_info = models.JSONField(default=list)
+    data_open = models.DateTimeField(
+        auto_now_add=True,
+    )
+    data_close = models.DateTimeField(
+        default=None,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return (f"History workout [ {self.workout.title} ]"
+                f" user [ {self.user.username} ]")
