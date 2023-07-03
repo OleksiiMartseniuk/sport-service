ea160e7882bb063e3c7d51b8c288f4dc91c908c6
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-07-03 21:52:35 +0300

    add WorkoutHistoryRetrieveSerializer

--- apps/history/views.py
+++ apps/history/views.py
@@ -1,12 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import WorkoutHistorySerializer
+from .serializers import (
+    WorkoutHistorySerializer,
+    WorkoutHistoryRetrieveSerializer,
+)
 from .models import WorkoutHistory
 
 
 class HistoryWorkoutView(viewsets.ReadOnlyModelViewSet):
 
+    queryset = WorkoutHistory.objects.all()
     serializer_class = WorkoutHistorySerializer
     permission_classes = [IsAuthenticated]
-    queryset = WorkoutHistory.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return WorkoutHistoryRetrieveSerializer
+        return super().get_serializer_class()
+
+    def get_queryset(self):
+        return WorkoutHistory.objects.filter(
+            user=self.request.user,
+        ).order_by('id')
